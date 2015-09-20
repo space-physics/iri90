3849fa54c638ce92f5076852e0778616c3aaa6b1
michael <hirsch617@gmail.com>
2015-09-20 03:18:57 -0400

    parameter passing

--- examples/demo_iri90.py
+++ examples/demo_iri90.py
@@ -7,4 +7,4 @@ from pyiri90.runiri90 import runiri
 if __name__ == '__main__':
     altkm=arange(100,1000,5)
     dtime = parse('2013-04-14T08:54:00')
-    outf,oarr= runiri(dtime,altkm)
+    outf,oarr= runiri(dtime,altkm,70,0,100,100,ap=4)
--- pyiri90/runiri90.py
+++ pyiri90/runiri90.py
@@ -15,21 +15,21 @@ import iri90 #fortran
 from msise00.runmsis import rungtd1d
 
 
-def runiri(dtime,altkm,glat,glon,f107,f107a,ap,mass=48):
+def runiri(dt,z,glat,glon,f107,f107a,ap,mass=48):
     jmag=0 #0:geographic 1: magnetic
     JF = array((1,1,1,1,0,1,1,1,1,1,1,0)).astype(bool) #Solomon 1993 version of IRI
     #JF = (1,1,1) + (0,0,0) +(1,)*14 + (0,1,0,1,1,1,1,0,0,0,1,1,0,1,0,1,1,1) #for 2013 version of IRI
 #%% call MSIS
-    dens,Tn = rungtd1d(dtime,altkm,glat,glon,f107a,f107,ap,mass,(1,)*25)
+    dens,Tn = rungtd1d(dt,z,glat,glon,f107a,f107,ap,mass,(1,)*25)
 #%% call IRI
     chdir(pyiri90.__path__[0])
     logging.debug(getcwd())
     outf,oarr = iri90.iri90(JF,jmag,glat,glon % 360., -f107,
-                       dtime.strftime('%m%d'),
-                       dtime.hour+dtime.minute//60+dtime.second//3600,
-                       altkm,'data/')
+                       dt.strftime('%m%d'),
+                       dt.hour+dt.minute//60+dt.second//3600,
+                       z,'data/')
 #%% arrange output
-    iono = DataFrame(index=altkm,
+    iono = DataFrame(index=z,
                      columns=['ne','Tn','Ti','Te','nO+','nH+','nHE+','nO2+','nNO+',
                               'nClusterIons','nN+'])
     iono['ne'] = outf[0,:]/1e6
