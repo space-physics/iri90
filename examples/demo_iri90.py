50cc10eb72f26d57082f4601140614f8bc27437f
michael <hirsch617@gmail.com>
2015-09-20 03:21:23 -0400

    parameter default

--- examples/demo_iri90.py
+++ examples/demo_iri90.py
@@ -6,5 +6,5 @@ from pyiri90.runiri90 import runiri
 
 if __name__ == '__main__':
     altkm=arange(100,1000,5)
-    dtime = parse('2013-04-14T08:54:00')
+    dtime = parse('1999-12-21T00:00:00Z')
     outf,oarr= runiri(dtime,altkm,70,0,100,100,ap=4)
