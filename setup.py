c09476038f59fabdba6a8707d8aef4073b834577
scienceopen <scienceopen@users.noreply.github.com>
2017-02-05 04:03:26 -0500

    prereq

--- setup.py
+++ setup.py
@@ -4,6 +4,14 @@ from numpy.distutils.core import setup,Extension
 from glob import glob
 from os.path import join
 
+try:
+    import conda.cli
+    conda.cli.main('install','--file','requirements.txt')
+except Exception as e:
+    print(e)
+    import pip
+    pip.main(['install','-r','requirements.txt'])
+
 #%% fortran data files
 iridata = glob(join('data','*.asc'))
 #%% install
