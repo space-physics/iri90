af5bf7d5f47216786c75d860953b008ce88d8ae8
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-04-16 01:24:29 -0400

    init

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import numpy as np
 from datetime import datetime
-from pyiri90 import runiri,plasmaprop
+from pyiri90 import runiri
 
 def test_iri90():
     altkm = np.arange(90,500,5)
@@ -20,12 +20,7 @@ def test_iri90():
         199106625536.0,0,0,176997889.0, 12948521984.0]
 
     np.testing.assert_allclose(I, It)
-#%%
-    wp,wH,reflectionheight = plasmaprop(iono,f0,B0)
 
-    np.testing.assert_almost_equal(wH,10552920.141612744)
-    np.testing.assert_almost_equal(wp[43],74761613.912995234)
-    np.testing.assert_almost_equal(reflectionheight,100.)
 
 if __name__ == '__main__':
     np.testing.run_module_suite()
