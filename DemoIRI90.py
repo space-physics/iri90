d807a5dd56127e8b9358227b9c766b2c6a302cca
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-02-27 14:46:27 -0500

    cleanup

--- DemoIRI90.py
+++ DemoIRI90.py
@@ -5,17 +5,11 @@ from matplotlib.pyplot import show
 #
 import pyiri90, pyiri90.plots
 
-f0 = 3.5e6 # radar frequency [Hz]
-B0 = 60e-6 # Geomagnetic field strength [Tesla] #TODO verify at altitude
-theta = 0.01 # radians off parallel from B
-
-fs = np.arange(1e6,8e6,0.05e6) # [Hz] radar frequency sweep
-
 if __name__ == '__main__':
     from argparse import ArgumentParser
     p = ArgumentParser()
-    p.add_argument('--alt',help='START STOP STEP altitude [km]',type=float,nargs=3,default=(85,500,1.))
-    p.add_argument('-t','--time',help='datetime of simulation',default='2012-07-21T12:35')
+    p.add_argument('--alt',help='START STOP STEP altitude [km]',type=float, nargs=3, default=(85,500,1.))
+    p.add_argument('-t','--time',help='date and time of simulation',default='2012-07-21T12:35')
     p.add_argument('-c','--latlon',help='geodetic coordinates of simulation',default=(65,-147.5),type=float)
     p.add_argument('--f107',type=float,default=200.)
     p.add_argument('--f107a',type=float,default=200.)
